2f56d0b93ef62c8d74445c2d21fe969ca6dea79e
Daniel <sydneydaniel76@gmail.com>
2024-05-02 19:56:33 +0100

    Implemented monkey emotion toggling

--- Week 6/Day 1/script.js
+++ Week 6/Day 1/script.js
@@ -2,3 +2,28 @@ const closedFace = document.querySelector('.closed');
 const openFace = document.querySelector('.open');
 
 // Add event listener here
+closedFace.addEventListener('click', function(){
+    closedFace.style.display ="none";
+    openFace.style.display ="block";
+    
+});
+
+openFace.addEventListener('click', function(){
+    openFace.style.display ="none";
+    closedFace.style.display ="block";
+    
+});
+
+
+//jQuery
+$(document).ready(function(){
+    $(".closed").click(function(){
+        $('.open').show();
+        $('.closed').hide();
+    });
+
+    $(".open").click(function(){
+        $(".closed").show();
+        $(".open").hide();
+    });
+});
